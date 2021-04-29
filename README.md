5e34b2d02d79d2cb4858e40f3bb05d1ad8a4f0bd
John Knutson <john.knutson@novu.com>
2021-04-29 11:32:35 -0500

    updates (#1)
    * updates
    
    * add Makefile, BuildVersion, other goodies
    
    * readme updates
    
    * refactor listServices into function
    
    * move serviceArns output to stdout

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # go-ecs-utils
+
+## building
+
+```sh
+go build -o ./bin/ecs-utils -v main.go
+```
+
+There is also a Makefile
+
+```sh
+# for OSX
+make build-darwin
+
+# cross-compile for RaspberryPi
+make build-raspi
+```
+
+# usage
+
+```sh
+./bin/ecs-utils
+```
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 	"regexp"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,56 +14,98 @@ import (
 )
 
 var (
-	ecsCluster          string = "tst-novu-apps"
-	serviceFilter       *regexp.Regexp
-	serviceFilterString string = `.*qa7.*` // TODO: get from flag
-	verbose             bool   = true      // TODO: get from flag
+	//BuildVersion is set during `go build` by `VERSION.txt`
+	BuildVersion         string
+	clusterName          string
+	servicesFilter       *regexp.Regexp
+	servicesFilterString string
+	verbose, version     bool
 )
 
-func main() {
-	var clusterServices []string
+func usage() {
+	println(`Usage: ecs-utils [options]
+Work with AWS ECS
+Options:`)
+	flag.PrintDefaults()
+	println(`For more information, see https://github.com/jknutson/one-wire-temp-go`)
+}
 
-	// TODO: get string from flag
-	serviceFilter = regexp.MustCompile(serviceFilterString)
+func initFlags() {
+	flag.StringVar(&clusterName, "clusterName", "default", "ECS cluster name")
+	flag.StringVar(&servicesFilterString, "servicesFilter", `.*`, "Service Filter (RegExp pattern)")
+	flag.BoolVar(&verbose, "verbose", false, "verbose output")
+	flag.BoolVar(&version, "version", false, "display version and exit")
 
-	sess, err := session.NewSession()
-	if err != nil {
-		log.Panicf("%s\n", err)
+	flag.Usage = usage
+	flag.Parse()
+
+	if version {
+		log.Printf("version: %s\n", BuildVersion)
+		os.Exit(0)
 	}
+
+	if verbose {
+		log.Printf("BuildVersion: %s\n", BuildVersion)
+		log.Printf("clusterName: %s\n", clusterName)
+		log.Printf("servicesFilter: %s\n", servicesFilter)
+	}
+
+}
+
+func listServices(sess *session.Session, filter *regexp.Regexp) ([]string, error) {
+	var services []string
+
 	svc := ecs.New(sess)
 	listServicesInput := &ecs.ListServicesInput{
-		Cluster: aws.String(ecsCluster),
+		Cluster: aws.String(clusterName),
 	}
 
 	for {
 		listServicesOutput, err := svc.ListServices(listServicesInput)
 		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				log.Println(aerr.Error())
-			} else {
-				log.Println(err.Error())
-			}
+			return nil, err
 		}
 		for _, serviceArn := range aws.StringValueSlice(listServicesOutput.ServiceArns) {
-			if serviceFilter.MatchString(serviceArn) {
-				clusterServices = append(clusterServices, serviceArn)
+			if servicesFilter.MatchString(serviceArn) {
+				services = append(services, serviceArn)
 			}
 		}
 
 		if listServicesOutput.NextToken != nil {
 			listServicesInput.NextToken = listServicesOutput.NextToken
 			if verbose {
-				log.Printf("NextToken found, will make another request\n")
+				log.Printf("listServices NextToken found, will make another request\n")
 			}
 		} else {
 			if verbose {
-				log.Printf("NextToken NOT found, breaking request loop\n")
+				log.Printf("listServices NextToken not found, breaking request loop\n")
 			}
 			break
 		}
 	}
+	return services, nil
+}
+
+func main() {
+	initFlags()
+
+	servicesFilter = regexp.MustCompile(servicesFilterString)
+
+	sess, err := session.NewSession()
+	if err != nil {
+		log.Panicf("%s\n", err)
+	}
+
+	clusterServices, err := listServices(sess, servicesFilter)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			log.Fatalf("%s\n", aerr.Error())
+		} else {
+			log.Fatalf("%s\n", err.Error())
+		}
+	}
 
 	for _, serviceArn := range clusterServices {
-		log.Printf("%s\n", serviceArn)
+		fmt.Printf("%s\n", serviceArn)
 	}
 }
