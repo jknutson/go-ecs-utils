c103d059adeb0f3c736cbf6a347e4416fb0f1115
John M. Knutson <john.knutson@novu.com>
2021-04-29 12:45:52 -0500

    updateService on xgame mode

--- cmd/updateService/main.go
+++ cmd/updateService/main.go
@@ -6,16 +6,17 @@ import (
 	"log"
 	"os"
 	// "regexp"
-	/*
-		"github.com/aws/aws-sdk-go/aws"
-		"github.com/aws/aws-sdk-go/aws/awserr"
-		"github.com/aws/aws-sdk-go/aws/session"
-		"github.com/aws/aws-sdk-go/service/ecs"
-	*/)
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecs"
+)
 
 var (
 	//BuildVersion is set during `go build` by `VERSION.txt`
 	BuildVersion     string
+	clusterName      string
 	serviceArn       string
 	confirmUpdate    bool
 	verbose, version bool
@@ -30,7 +31,8 @@ Options:`)
 }
 
 func initFlags() {
-	flag.StringVar(&serviceArn, "serviceArn", "", "ECS Service ARN. '-' reads from STDIN (required")
+	flag.StringVar(&clusterName, "clusterName", "default", "ECS Cluster name")
+	flag.StringVar(&serviceArn, "serviceArn", "", "ECS Service ARN; '-' reads from STDIN (required)")
 	flag.BoolVar(&confirmUpdate, "confirmUpdate", false, "confirm update")
 	flag.BoolVar(&verbose, "verbose", false, "verbose output")
 	flag.BoolVar(&version, "version", false, "display version and exit")
@@ -45,6 +47,7 @@ func initFlags() {
 
 	if verbose {
 		log.Printf("BuildVersion: %s\n", BuildVersion)
+		log.Printf("clusterName: %s\n", clusterName)
 		log.Printf("serviceArn: %s\n", serviceArn)
 		log.Printf("confirmUpdate: %t\n", confirmUpdate)
 	}
@@ -52,39 +55,65 @@ func initFlags() {
 	// TODO: exit/error if serviceArn == ""
 }
 
-func updateService(serviceArn string) error {
-	// TODO: validate ARN regex
+func updateService(sess *session.Session, serviceArn string) error {
+	if confirmUpdate {
+		// TODO: validate ARN regex
+		if verbose {
+			log.Printf("updating %s\n", serviceArn)
+		}
+
+		svc := ecs.New(sess)
+		updateServiceInput := &ecs.UpdateServiceInput{
+			Cluster:            aws.String(clusterName),
+			ForceNewDeployment: aws.Bool(true),
+			Service:            aws.String(serviceArn),
+		}
+
+		_, err := svc.UpdateService(updateServiceInput)
+		if err != nil {
+			return err
+		}
+		log.Printf("updated %s\n", serviceArn)
+		return nil
+	}
 	if verbose {
-		log.Printf("%s\n", serviceArn)
+		log.Printf("skipping update: confirmUpdate = %t\n", confirmUpdate)
 	}
 	return nil
+}
+
+// TODO: move to pkg/ and share with other cmds
+func awsError(err error) {
+	if aerr, ok := err.(awserr.Error); ok {
+		log.Fatalf("%s\n", aerr.Error())
+	} else {
+		log.Fatalf("%s\n", err.Error())
+	}
 }
 
 func main() {
 	initFlags()
 
-	/*
-		sess, err := session.NewSession()
-		if err != nil {
-			log.Panicf("%s\n", err)
-		}
-	*/
+	sess, err := session.NewSession()
+	if err != nil {
+		log.Panicf("%s\n", err)
+	}
 
-	if serviceArn == "-" { // read from STDIN
+	if serviceArn == "-" {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
-			err := updateService(scanner.Text())
+			err := updateService(sess, scanner.Text())
 			if err != nil {
-				log.Fatalf("%s\n", err)
+				awsError(err)
 			}
 		}
 		if err := scanner.Err(); err != nil {
 			log.Println(err)
 		}
 	} else {
-		err := updateService(serviceArn)
+		err := updateService(sess, serviceArn)
 		if err != nil {
-			log.Fatalf("%s\n", err)
+			awsError(err)
 		}
 	}
 }
