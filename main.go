551a2e16e6c05d0b17c34cb3284afc9c5fca3cc9
John M. Knutson <john.knutson@novu.com>
2021-04-29 10:51:41 -0500

    refactor listServices into function

--- main.go
+++ main.go
@@ -14,11 +14,11 @@ import (
 
 var (
 	//BuildVersion is set during `go build` by `VERSION.txt`
-	BuildVersion        string
-	clusterName         string
-	serviceFilter       *regexp.Regexp
-	serviceFilterString string
-	verbose, version    bool
+	BuildVersion         string
+	clusterName          string
+	servicesFilter       *regexp.Regexp
+	servicesFilterString string
+	verbose, version     bool
 )
 
 func usage() {
@@ -31,7 +31,7 @@ Options:`)
 
 func initFlags() {
 	flag.StringVar(&clusterName, "clusterName", "default", "ECS cluster name")
-	flag.StringVar(&serviceFilterString, "serviceFilter", `.*`, "Service Filter (RegExp pattern)")
+	flag.StringVar(&servicesFilterString, "servicesFilter", `.*`, "Service Filter (RegExp pattern)")
 	flag.BoolVar(&verbose, "verbose", false, "verbose output")
 	flag.BoolVar(&version, "version", false, "display version and exit")
 
@@ -42,18 +42,18 @@ func initFlags() {
 		log.Printf("version: %s\n", BuildVersion)
 		os.Exit(0)
 	}
+
+	if verbose {
+		log.Printf("BuildVersion: %s\n", BuildVersion)
+		log.Printf("clusterName: %s\n", clusterName)
+		log.Printf("servicesFilter: %s\n", servicesFilter)
+	}
+
 }
 
-func main() {
-	initFlags()
-	var clusterServices []string
+func listServices(sess *session.Session, filter *regexp.Regexp) ([]string, error) {
+	var services []string
 
-	serviceFilter = regexp.MustCompile(serviceFilterString)
-
-	sess, err := session.NewSession()
-	if err != nil {
-		log.Panicf("%s\n", err)
-	}
 	svc := ecs.New(sess)
 	listServicesInput := &ecs.ListServicesInput{
 		Cluster: aws.String(clusterName),
@@ -62,28 +62,45 @@ func main() {
 	for {
 		listServicesOutput, err := svc.ListServices(listServicesInput)
 		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				log.Println(aerr.Error())
-			} else {
-				log.Println(err.Error())
-			}
+			return nil, err
 		}
 		for _, serviceArn := range aws.StringValueSlice(listServicesOutput.ServiceArns) {
-			if serviceFilter.MatchString(serviceArn) {
-				clusterServices = append(clusterServices, serviceArn)
+			if servicesFilter.MatchString(serviceArn) {
+				services = append(services, serviceArn)
 			}
 		}
 
 		if listServicesOutput.NextToken != nil {
 			listServicesInput.NextToken = listServicesOutput.NextToken
 			if verbose {
-				log.Printf("NextToken found, will make another request\n")
+				log.Printf("listServices NextToken found, will make another request\n")
 			}
 		} else {
 			if verbose {
-				log.Printf("NextToken NOT found, breaking request loop\n")
+				log.Printf("listServices NextToken not found, breaking request loop\n")
 			}
 			break
+		}
+	}
+	return services, nil
+}
+
+func main() {
+	initFlags()
+
+	servicesFilter = regexp.MustCompile(servicesFilterString)
+
+	sess, err := session.NewSession()
+	if err != nil {
+		log.Panicf("%s\n", err)
+	}
+
+	clusterServices, err := listServices(sess, servicesFilter)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			log.Fatalf("%s\n", aerr.Error())
+		} else {
+			log.Fatalf("%s\n", err.Error())
 		}
 	}
 
